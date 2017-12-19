9a6f939d99e51f4e119d9906c1fce55f9d3699bd
User <User@K3-504-05>
2017-12-19 21:00:17 +0500

    11

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слова");
+            string s = Console.ReadLine();
+            char[] separator = { ' ' };
+            string[] arr = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            for(int i = 0; i < arr.Length; i++)
+            {
+                if (IsKeyWord(arr[i]) == true)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+
+            }
+
+            //s = String.Join(" ", arr);
+            //Console.WriteLine(s);
+
+        }
+
+        static bool IsKeyWord(string word)
+        {
+            string[] arr = { "if", "bool", "else", "switch", "static", "using" };
+            for (int i = 0; i < arr.Length; i ++)
+            {
+                if(String.Compare(word, arr[i]) == 0)
+                {
+                    return true;
+                }
+
+            }
+            return false;
+        }
+    }
+}
