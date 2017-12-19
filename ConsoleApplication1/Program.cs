611382143453c333398f3c12d6a8cdd45f6d9afb
User <User@K3-504-05>
2017-12-19 20:39:45 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] separator = { ' ' };
+            string[] arr = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            char[] symb = new char[arr.Length];
+            Array.Sort(arr);
+            s = String.Join(" ", arr);
+            Console.WriteLine(s);
+        }
+    }
+}
---
+++ ConsoleApplication6/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            char[] separator = {' '};
+            
+            string[] arr = s.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+            char[] symb = new char[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                symb = arr[i].ToCharArray();
+                Array.Reverse(symb);
+                Console.Write(arr[i] + " ");
+                arr[i] = new string (symb);
+
+            }
+            s = String.Join(" ", arr);
+            Console.WriteLine();
+            Console.WriteLine(s);
+            
+        }
+    }
+}
